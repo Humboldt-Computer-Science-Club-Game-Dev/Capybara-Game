3ae1eba4afa0af5c1893a5e6c2be2d8a03b2400c
Anthony Cavuoti <anthonycavuoti@gmail.com>
2023-02-01 11:39:27 -0800

    Add UI elements and add begining of event system for health

---
+++ Assets/scripts/Health_UI_Player.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* PRE-Requisites. Player must be instantiated in scene
+TODOS: Make it so that player prefab comes with the health UI prefab attached to it */
+
+public class Health_UI_Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
