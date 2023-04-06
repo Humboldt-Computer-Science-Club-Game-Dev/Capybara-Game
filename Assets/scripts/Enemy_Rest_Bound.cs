429261caa66ec8eb917c011ee3218253291f4d52
Anthony Cavuoti <anthonycavuoti@gmail.com>
2023-04-06 14:03:10 -0700

    clean enemy rest bound script

--- Assets/scripts/Enemy_Rest_Bound.cs
+++ Assets/scripts/Enemy_Rest_Bound.cs
@@ -2,18 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/* 
+    This is a super simple script that is meant to just store an 
+    integer that will get accessed by enemies as they position there 
+    selfs on screen. Each enemy has a designated rest bound and will 
+    continue to move left until they collide with an collider that 
+    has this script and has the same rest bound as the enemies
+    designated rest bound. 
+*/
 public class Enemy_Rest_Bound : MonoBehaviour
 {
     public int restBound = 1;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
