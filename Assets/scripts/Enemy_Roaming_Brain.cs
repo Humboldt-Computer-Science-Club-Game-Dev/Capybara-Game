8999a5b552c84775ce07e52e64de9cbf2c025446
Anthony Cavuoti <anthonycavuoti@gmail.com>
2023-04-06 14:32:20 -0700

    Clean enemy roamin brain

--- Assets/scripts/Enemy_Roaming_Brain.cs
+++ Assets/scripts/Enemy_Roaming_Brain.cs
@@ -1,70 +1,79 @@
 using UnityEngine;
 using System;
 
+/* 
+    Enemy movement brain is required because enemy roaming 
+    brain is an extension of Enemy movement brain 
+*/
 [RequireComponent(typeof(Enemy_Movement_Brain))]
-
 
 public class Enemy_Roaming_Brain : MonoBehaviour
 {
     public float MovementSpeed = 4;
-    Transform PathContainer;
-
     public Transform[] _points;
-
-    private int _currentTargetIdx = 1;
-
-    Enemy_Movement_Brain movementBrain;
-
     public float roamCooldown = 10f;
     public float roamCooldownTimer;
-
+    Transform PathContainer;
+    int _currentTargetIdx = 0;
+    Enemy_Movement_Brain movementBrain;
     Health health;
-
     const string ROAMING_PREFAB_PATH_BASE = "prefabs/patrol_path_container_var_0";
-
-    private void Awake()
-    {
-    }
-
     void Start()
     {
-        movementBrain = GetComponent<Enemy_Movement_Brain>();
-        //TODO: make roamCooldownTimer init to a random value between 0 and roamCooldown
-        roamCooldownTimer = Random_Number_Generator.randomPositiveFloatUnder(roamCooldown);
-        health = GetComponent<Health>();
-    }
+        getComponents();
 
+        // this timer is initialized to a random value so that not all enemies roam at the same time
+        roamCooldownTimer = Random_Number_Generator.randomPositiveFloatUnder(roamCooldown);
+    }
     private void Update()
     {
         handleTimer();
-        if (!isTimeToRoam()) return;
+        if (isTimeToRoam()) roam();
+
+               
+    }
+    void roam(){
+        //Will set movement brain's movement state to roaming
+        movementBrain.startRoaming();
 
         roamCooldownTimer = 0;
+
         //Instantiates PathContainer prefab at this position
         if(PathContainer == null){
             PathContainer = Instantiate((GameObject)Resources.Load(ROAMING_PREFAB_PATH_BASE + createPathVariantNumber(), typeof(GameObject)), transform.position, transform.rotation).transform;
             _points = PathContainer.GetComponentsInChildren<Transform>();
         }
-        //Will set movement brain's movement state to roaming and make this function return false
-        movementBrain.startRoaming();
 
+        // Moves the enemy towards the current target point in path array
+        transform.position = Vector3.MoveTowards(transform.position, _points[_currentTargetIdx].position, MovementSpeed * Time.deltaTime);
+
+        // Distance is the distance between the enemy and the current target point in path array
         var distance = Vector3.Distance(transform.position, _points[_currentTargetIdx].position);
-        if (Mathf.Abs(distance) < 0.1f)
-        {
-            _currentTargetIdx++;
-            if (_currentTargetIdx >= _points.Length)
-            {
-                // we reached the end of the path
-                _currentTargetIdx = 1;
-                movementBrain.onFinishedRoaming();
-                Destroy(PathContainer.gameObject);
-            }
-        }
-        transform.position = Vector3.MoveTowards(transform.position, _points[_currentTargetIdx].position, MovementSpeed * Time.deltaTime);       
+
+        if (Mathf.Abs(distance) < 0.1f) _currentTargetIdx++;
+        if (_currentTargetIdx >= _points.Length) finishRoaming();
     }
+    void finishRoaming(){
+        _currentTargetIdx = 0;
+
+        //Will set movement brain's movement state to oscillating
+        movementBrain.onFinishedRoaming();
+
+        Destroy(PathContainer.gameObject);
+    }
+    void getComponents(){
+        movementBrain = GetComponent<Enemy_Movement_Brain>();
+        health = GetComponent<Health>();
+    }
+    /* 
+        There are 6 different path variants, so this method will return a random number between 1 and 6. 
+        This number will be used to instantiate the correct path prefab. 
+    */
     private int createPathVariantNumber(){
         return (int)Math.Round((double)UnityEngine.Random.Range(1f, 6.4f));
     }
+
+    // This method will increment the roamCooldownTimer by 1 every second if the enemy is oscillating
     private void handleTimer(){
         if (!(movementBrain.getMovementState() == Enemy_Movement_Brain.Movement.oscillating)) return;
         roamCooldownTimer += (1f * Time.deltaTime);
@@ -73,6 +82,7 @@ public class Enemy_Roaming_Brain : MonoBehaviour
         return (roamCooldownTimer >= roamCooldown || movementBrain.isRoaming() && !health.isDead());
     }
 
+    // This method will draw the path in the scene view
     private void OnDrawGizmosSelected()
     {
         if (_points == null || _points.Length == 0) return;
