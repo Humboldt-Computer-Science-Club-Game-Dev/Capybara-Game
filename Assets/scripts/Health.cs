bca8e9d646740551b85cbe83a29b7c52fcb4510b
Anthony Cavuoti <anthonycavuoti@gmail.com>
2023-04-07 11:45:12 -0700

    Clean health script

--- Assets/scripts/Health.cs
+++ Assets/scripts/Health.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/* This script is a generic health management system that is meant to be reused when necessary in order to prevent duplicate code  */
 public class Health : MonoBehaviour
 {
-    public int health = 100;
-    public int maxHealth = 100;
+    public int health = 3;
+    public int maxHealth = 3;
     public int minHealth = 0;
 
    
@@ -22,25 +23,11 @@ public class Health : MonoBehaviour
     {
         if(isDead()) return;
         health += heal;
-        if (health > maxHealth)
-        {
-            health = maxHealth;
-        }
+        if (health > maxHealth) health = maxHealth;
     }
     public bool isDead()
     {
         if (health == minHealth) return true;
         else return false;
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
