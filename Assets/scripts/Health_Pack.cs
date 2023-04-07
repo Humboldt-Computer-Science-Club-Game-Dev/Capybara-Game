3cde8dceeb1290847e5a5081d9802e582d72e98c
Anthony Cavuoti <anthonycavuoti@gmail.com>
2023-04-07 11:56:16 -0700

    Clean health pack script

--- Assets/scripts/Health_Pack.cs
+++ Assets/scripts/Health_Pack.cs
@@ -5,41 +5,61 @@ using UnityEngine;
 public class Health_Pack : MonoBehaviour
 {
     public int healAmount = 1;
+    [SerializeField, Tooltip("Set gravity seed greater than 0 to make the health pack move towards the gravity target")]
     public float gravitySpeed = 0f;
+    [SerializeField, Tooltip("Enter the name of the object in scene view that the health pack will move towards")]
+    public string gravityTarget = "capy";
+    [SerializeField, Tooltip("Enter the tag of the object that the health pack will heal")]
+    public string gravityTargetTag = "Player";
     private BoxCollider2D boxCollider;
     private Transform playerLocation;
-    // Start is called before the first frame update
+
     void Start()
     {
         boxCollider = GetComponent<BoxCollider2D>();
-        playerLocation = GameObject.Find("capy").GetComponent<Transform>();
+        playerLocation = GameObject.Find(gravityTarget).GetComponent<Transform>();
     }
 
     public float lifetime = 10f;
     float timer = 0f;
 
-    // Update is called once per frame
     void Update()
     {
+        deSpawnAfterLifetime();
+        healOnTargetCollision();
+    }
+
+    // Just in case you want to change the gravity target at runtime
+    public void setGravityTarget(string target){
+        gravityTarget = target;
+    }
+    // Just in case you want to change the gravity target tag at runtime
+    public void setGravityTargetTag(string targetTag){
+        gravityTargetTag = targetTag;
+    }
+
+    void FixedUpdate(){
+        // Moves the health pack towards the gravity target at the speed of gravitySpeed
+        transform.position = Vector2.MoveTowards(transform.position, playerLocation.position, gravitySpeed * Time.deltaTime);
+    }
+
+    void deSpawnAfterLifetime(){
         timer += Time.deltaTime;
         if(timer > lifetime){
             Destroy(this.gameObject);
         }
+    }
 
+    void healOnTargetCollision(){
         Collider2D[] hits = Physics2D.OverlapBoxAll(transform.position, boxCollider.size, 0);
         foreach (Collider2D hit in hits){
-            
         	if (hit == boxCollider)
         	continue;
 
-           if(hit.gameObject.tag == "Player"){
+           if(hit.gameObject.tag == gravityTargetTag){
               hit.gameObject.GetComponent<player>().healPlayer(healAmount);
                Destroy(this.gameObject);
            }
         }
-    }
-
-    void FixedUpdate(){
-        transform.position = Vector2.MoveTowards(transform.position, playerLocation.position, gravitySpeed * Time.deltaTime);
     }
 }
