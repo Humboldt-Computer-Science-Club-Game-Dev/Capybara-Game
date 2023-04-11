c8e3550a53a30e7060eb2a5874b77285a7e59cef
Anthony Cavuoti <anthonycavuoti@gmail.com>
2023-04-11 13:03:00 -0700

    Clean player ui death script

--- Assets/scripts/Player_UI_Death.cs
+++ Assets/scripts/Player_UI_Death.cs
@@ -2,31 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/* 
+    Note because this game object this script is attached starts off as inactive. 
+    This script dose not have a place where it is set to active. This game object
+    is set to active by the player script upon player death.
+*/
 public class Player_UI_Death : MonoBehaviour
 {
-    private bool isShown = false;
-    private float timeShown = 0f;
+    [SerializeField, Tooltip("Length of time to show the Cappy Died Screen")]
     public float timeToBeShown = 3f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    bool isShown = false;
+    float timeShown = 0f;
     void Update()
     {
-        if(isShown){
-            timeShown += Time.deltaTime;
-            if(timeShown >= timeToBeShown){
-                isShown = false;
-                timeShown = 0f;
-                this.gameObject.SetActive(false);
-                GameObject.Find("capy").GetComponent<player>().playerDeathUIShownEnough();
-            }
-        }
+        if(isShown) if(hasShownLongEnough()) deactivate();
     }
     public void beginDeathText(){
         isShown = true;
+    }
+    bool hasShownLongEnough(){
+        timeShown += Time.deltaTime;
+        return timeShown >= timeToBeShown;
+    }
+    void deactivate(){
+        isShown = false;
+        timeShown = 0f;
+        this.gameObject.SetActive(false);
+        GameObject.Find("capy").GetComponent<player>().playerDeathUIShownEnough();  
     }
 }
