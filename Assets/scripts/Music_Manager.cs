4025dc167a73420dfaa64cf18d47a339d4ac7adc
Anthony Cavuoti <anthonycavuoti@gmail.com>
2023-05-10 12:55:18 -0700

    Add specification to music manager

--- Assets/scripts/Music_Manager.cs
+++ Assets/scripts/Music_Manager.cs
@@ -1,6 +1,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/* 
+    Music transition system, when a force play transition is played, 
+    the current song transitions to lower volume. The next music clip
+    is then played at 0 volume and then transitions to full volume.
+
+    When the current music request is close to finishing and it has a property from 
+    the MusicSettings struct called transitionPlay set to true, when that song approaches
+    the end of its duration, that song will transition to volume 0 nad the next song in the
+    queue will play at volume 0 and then transition to full volume if that next song has 
+    transitionPlay set.
+*/
 
 public class Music_Manager : MonoBehaviour
 {
@@ -38,18 +49,20 @@ public class Music_Manager : MonoBehaviour
             {
                 if (musicSettings.transitionPlay)
                 {
+                    // Dose not work.
+                    EnqueueAtBeginning(new MusicRequest(musicClip, musicSettings));
                     instance.musicQueue.Enqueue(new MusicRequest(musicClip, musicSettings));
                     instance.StartCoroutine(instance.TransitionPlay(musicSettings.transitionDuration));
                 }
                 else
                 {
-                    instance.musicQueue.Enqueue(new MusicRequest(instance.audioSource.clip, musicSettings));
                     instance.audioSource.clip = musicClip;
                     instance.audioSource.Play();
                 }
             }
             else
             {
+                Debug.Log("Music_Manager: " + musicName + " added to queue.");
                 instance.musicQueue.Enqueue(new MusicRequest(musicClip, musicSettings));
             }
         }
@@ -79,6 +92,20 @@ public class Music_Manager : MonoBehaviour
         }
 
         instance.audioSource.Stop();
+    }
+
+     static void EnqueueAtBeginning(MusicRequest value)
+    {
+        // Create a temporary queue and add the new value
+        Queue<MusicRequest> tempQueue = new Queue<MusicRequest>();
+        tempQueue.Enqueue(value);
+
+        // Add all items from the original queue to the temporary queue
+        while (instance.musicQueue.Count > 0)
+            tempQueue.Enqueue(instance.musicQueue.Dequeue());
+
+        // Replace the original queue with the temporary queue
+        instance.musicQueue = tempQueue;
     }
 
     public static void ClearQueue()
@@ -131,7 +158,8 @@ public class Music_Manager : MonoBehaviour
 
     void manageMusicQueue(){
         /* determines weather or not to play the next music in the queue */
-        if(audioSource.isPlaying && audioSource.timeSamples < previousSamplePosition && musicQueue.Count > 0){
+        if((audioSource.isPlaying && audioSource.timeSamples < previousSamplePosition && musicQueue.Count > 0) || (!audioSource.isPlaying && musicQueue.Count > 0)){
+            Debug.Log("Music_Manager: playing next music in queue.");
             MusicRequest nextRequest = musicQueue.Dequeue();
             PlayMusicRequest(nextRequest);
         }
--- Assets/scripts/tests/Music_Manager_Tester.cs
+++ Assets/scripts/tests/Music_Manager_Tester.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Music_Manager_Tester : MonoBehaviour
 {
+    float timer = 0;
+    float maxTime = 5;
+    bool played = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,12 +17,30 @@ public class Music_Manager_Tester : MonoBehaviour
         musicSettings.loop = true;
         musicSettings.volume = 1;
         musicSettings.pitch = 1;
+        
+        
+
         Music_Manager.PlayMusic("cappy_defense_force_2", musicSettings);
+        Music_Manager.PlayMusic("The_geese_kind_3", musicSettings);
+
+        Music_Manager.PlayMusic("The_geese_kind_3", musicSettings);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        if(timer > maxTime && !played){
+            MusicSettings musicSettings2 = new MusicSettings();
+            musicSettings2.forcePlay = true;
+            musicSettings2.transitionPlay = true;
+            musicSettings2.transitionDuration = 2f;
+            musicSettings2.loop = true;
+            musicSettings2.volume = 1;
+            musicSettings2.pitch = 1;
+            Music_Manager.PlayMusic("The_geese_kind_1", musicSettings2);
+            played = true;
+        }
     }
 }
