71436a547930c5a854a67c5f76b62fac48ce0887
Anthony Cavuoti <anthonycavuoti@gmail.com>
2023-06-06 22:55:39 -0700

    Add levels and its dialog

--- Assets/scripts/Wave_Music_Manager.cs
+++ Assets/scripts/Wave_Music_Manager.cs
@@ -5,18 +5,54 @@ using UnityEngine;
 public class Wave_Music_Manager : MonoBehaviour
 {
     public MusicActionSet[] musicActionSets;
+    int currentWave = -1;
 
     void Start()
     {
         Event_System.onWaveEnds += onWaveEnds;
+        Event_System.onSequenceEnds += onSequenceEnds;
     }
 
     void onWaveEnds(int waveIndex)
     {
+        currentWave = waveIndex;
         for (int i = 0; i < musicActionSets.Length; i++)
         {
             MusicActionSet currentSet = musicActionSets[i];
-            if (currentSet.playOnWaveIndex != waveIndex) continue;
+            if (currentSet.playOnWaveIndex != waveIndex || currentSet.playOnSequenceEnd) continue;
+            MusicAction[] actions = currentSet.actions;
+            for (int j = 0; j < actions.Length; j++)
+            {
+                MusicAction currentAction = actions[j];
+                switch (currentAction.actionType)
+                {
+                    case MusicActionType.play:
+                        Music_Manager.PlayMusic(currentAction.musicName, currentAction.musicSettings);
+                        break;
+                    case MusicActionType.stop:
+                        Music_Manager.StopMusic();
+                        break;
+                    case MusicActionType.pause:
+                        Music_Manager.PauseMusic();
+                        break;
+                    case MusicActionType.resume:
+                        Music_Manager.ResumeMusic();
+                        break;
+                    case MusicActionType.clear:
+                        Music_Manager.ClearQueue();
+                        break;
+                }
+            }
+        }
+    }
+
+    void onSequenceEnds()
+    {
+        Debug.Log("Sequence ends");
+        for (int i = 0; i < musicActionSets.Length; i++)
+        {
+            MusicActionSet currentSet = musicActionSets[i];
+            if (currentWave != currentSet.playOnWaveIndex || !currentSet.playOnSequenceEnd) continue;
             MusicAction[] actions = currentSet.actions;
             for (int j = 0; j < actions.Length; j++)
             {
@@ -56,6 +92,7 @@ public class Wave_Music_Manager : MonoBehaviour
     {
         public MusicAction[] actions;
         public int playOnWaveIndex;
+        public bool playOnSequenceEnd;
     }
 
     public enum MusicActionType
